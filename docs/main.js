2c1a70d2563f535ea8beabe69959719cfe813405
it-opes <programmer@it-opes.com>
2019-02-24 17:24:16 +0100

    Deploying

--- docs/main.js
+++ docs/main.js
@@ -8,9 +8,9 @@
 /***/ (function(module, exports, __webpack_require__) {
 
 var map = {
-	"./pages/movie-list-page/movie-list-page.module": [
-		"./src/app/pages/movie-list-page/movie-list-page.module.ts",
-		"pages-movie-list-page-movie-list-page-module"
+	"./movie-page/movie-page.module": [
+		"./src/app/movie-page/movie-page.module.ts",
+		"movie-page-movie-page-module"
 	]
 };
 function webpackAsyncContext(req) {
@@ -59,7 +59,7 @@ var routes = [
     },
     {
         path: 'movie',
-        loadChildren: './pages/movie-list-page/movie-list-page.module#MovieListPageModule'
+        loadChildren: './movie-page/movie-page.module#MoviePageModule'
     }
 ];
 var AppRoutingModule = /** @class */ (function () {
@@ -96,7 +96,7 @@ module.exports = "\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<!--The content below is only a placeholder and can be replaced.-->\n<div style=\"text-align:center\">\n  <h1>\n    Witaj w {{ title }}!\n  </h1>\n  <img width=\"300\" alt=\"Angular Logo\" src=\"data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==\">\n</div>\n<h2>Here are some links to help you start: </h2>\n<p><button mat-button>Click me!</button></p>\n<p><mat-checkbox>Check me!</mat-checkbox></p>\n<ul>\n  <li>\n    <h2><a target=\"_blank\" rel=\"noopener\" href=\"https://angular.io/tutorial\">Tour of Heroes</a></h2>\n  </li>\n  <li>\n    <h2><a target=\"_blank\" rel=\"noopener\" href=\"https://angular.io/cli\">CLI Documentation</a></h2>\n  </li>\n  <li>\n    <h2><a target=\"_blank\" rel=\"noopener\" href=\"https://blog.angular.io/\">Angular blog</a></h2>\n  </li>\n</ul>\n\n"
+module.exports = "<router-outlet></router-outlet>\n\n"
 
 /***/ }),
 
@@ -147,8 +147,9 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
 /* harmony import */ var _app_component__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./app.component */ "./src/app/app.component.ts");
 /* harmony import */ var _angular_platform_browser_animations__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @angular/platform-browser/animations */ "./node_modules/@angular/platform-browser/fesm5/animations.js");
-/* harmony import */ var _angular_material__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! @angular/material */ "./node_modules/@angular/material/esm5/material.es5.js");
-/* harmony import */ var _app_routing_module__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./app-routing.module */ "./src/app/app-routing.module.ts");
+/* harmony import */ var _app_routing_module__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./app-routing.module */ "./src/app/app-routing.module.ts");
+/* harmony import */ var _shared_services_movie_request_service__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./shared/services/movie/request.service */ "./src/app/shared/services/movie/request.service.ts");
+/* harmony import */ var _angular_common_http__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! @angular/common/http */ "./node_modules/@angular/common/fesm5/http.js");
 
 
 
@@ -156,7 +157,7 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
-//import { MovieListPageComponent } from './pages/movie-list-page/components/@init/movie-list-page.component';
+
 var AppModule = /** @class */ (function () {
     function AppModule() {
     }
@@ -167,16 +168,73 @@ var AppModule = /** @class */ (function () {
             ],
             imports: [
                 _angular_platform_browser__WEBPACK_IMPORTED_MODULE_1__["BrowserModule"],
-                _app_routing_module__WEBPACK_IMPORTED_MODULE_6__["AppRoutingModule"],
+                _app_routing_module__WEBPACK_IMPORTED_MODULE_5__["AppRoutingModule"],
                 _angular_platform_browser_animations__WEBPACK_IMPORTED_MODULE_4__["BrowserAnimationsModule"],
-                _angular_material__WEBPACK_IMPORTED_MODULE_5__["MatButtonModule"],
-                _angular_material__WEBPACK_IMPORTED_MODULE_5__["MatCheckboxModule"]
+                _angular_common_http__WEBPACK_IMPORTED_MODULE_7__["HttpClientModule"]
             ],
-            providers: [],
+            providers: [_shared_services_movie_request_service__WEBPACK_IMPORTED_MODULE_6__["RequestService"]],
             bootstrap: [_app_component__WEBPACK_IMPORTED_MODULE_3__["AppComponent"]]
         })
     ], AppModule);
     return AppModule;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/shared/services/movie/request.service.ts":
+/*!**********************************************************!*\
+  !*** ./src/app/shared/services/movie/request.service.ts ***!
+  \**********************************************************/
+/*! exports provided: RequestService */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "RequestService", function() { return RequestService; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _angular_common_http__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/common/http */ "./node_modules/@angular/common/fesm5/http.js");
+
+
+
+var API_URL = 'http://vps135320.ovh.net:1234';
+var API_METHODS = {
+    fetchList: API_URL + "/list/:page",
+    fetchMovie: API_URL + "/movie/:id"
+};
+var RequestService = /** @class */ (function () {
+    function RequestService(http) {
+        this.http = http;
+    }
+    RequestService.prototype.fetchList = function (page) {
+        if (page === void 0) { page = '1'; }
+        return this.makeRequest(API_METHODS.fetchList, {
+            page: page
+        });
+    };
+    RequestService.prototype.fetchMovie = function (id) {
+        return this.makeRequest(API_METHODS.fetchMovie, {
+            id: id
+        });
+    };
+    RequestService.prototype.makeRequest = function (url, params) {
+        if (params === void 0) { params = {}; }
+        Object.entries(params)
+            .forEach(function (_a) {
+            var key = _a[0], value = _a[1];
+            url = url.replace(":" + key, value);
+        });
+        return this.http.get(url);
+    };
+    RequestService = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["Injectable"])({
+            providedIn: 'root'
+        }),
+        tslib__WEBPACK_IMPORTED_MODULE_0__["__metadata"]("design:paramtypes", [_angular_common_http__WEBPACK_IMPORTED_MODULE_2__["HttpClient"]])
+    ], RequestService);
+    return RequestService;
 }());
 
 
